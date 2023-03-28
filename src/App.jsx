fa8ff2d03bedcf58d7bb54ec4e19cfe0f9ca0787
nefzina <nefzi.amani@gmail.com>
2023-03-28 18:07:27 +0200

    usage of props

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import { useState } from 'react'
 import './App.css'
 import PokemonCard from './composants/PokemonCard'
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },];
+
 const App = () => {
+  const pokemon = pokemonList[0];
   return (
     <div>
-      <PokemonCard />
+      {/* <PokemonCard pokemon={pokemonList[0]}/> */}
+      <PokemonCard pokemon={pokemon}/>
     </div>
   );
 }
--- src/composants/PokemonCard.jsx
+++ src/composants/PokemonCard.jsx
@@ -1,16 +1,6 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
-const PokemonCard = () => {
-    const pokemon = pokemonList[1];
+
+const PokemonCard = ({pokemon}) => {
     return (
     !pokemon.imgSrc ?
     <figure>
