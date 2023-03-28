af4995de653f355aa029edd0f62f673945ad7666
nefzina <nefzi.amani@gmail.com>
2023-03-29 00:03:25 +0200

    type checking with PropTypes

--- src/composants/PokemonCard.jsx
+++ src/composants/PokemonCard.jsx
@@ -1,4 +1,4 @@
-
+import PropTypes from 'prop-types';
 
 const PokemonCard = ({pokemon}) => {
     return (
@@ -16,4 +16,12 @@ const PokemonCard = ({pokemon}) => {
     </figure>
     );
 }
+
+PokemonCard.propTypes = {
+      pokemon: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        imgSrc: PropTypes.string,
+      }).isRequired,
+  }
+
 export default PokemonCard;
